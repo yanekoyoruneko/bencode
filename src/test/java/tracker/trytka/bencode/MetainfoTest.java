4cf0055a0a4743626e203341111f0ca837f82248
yan3ku <wisniewskijas@outlook.com>
2025-03-11 03:07:50 +0100

    Add tests

--- src/test/java/tracker/trytka/bencode/MetainfoTest.java
+++ src/test/java/tracker/trytka/bencode/MetainfoTest.java
@@ -1,5 +1,7 @@
 package tracker.trytka.bencode;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
@@ -18,12 +20,16 @@ public class MetainfoTest {
     var decoder = new Decoder(ByteBuffer.wrap(raw));
     var bdict = decoder.parse();
     var meta = Metainfo.of((BDict) bdict);
+    System.out.println("INFO HASH VALUE:  " + meta.info_hash_str());
+    System.out.println("TORRENT URL: " + meta.info_hash_url());
     if (meta.isMultipleFileMode()) {
       System.out.println("FILES:");
       for (var file : meta.getFiles().entrySet()) {
         System.out.println(file);
       }
     }
+    byte[] parsed = Metainfo.decodeInfoHashURL(meta.info_hash_url());
+    assertArrayEquals(parsed, meta.info_hash());
     System.out.println(meta);
   }
 }
